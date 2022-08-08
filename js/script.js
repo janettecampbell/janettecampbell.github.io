823f15c5bba2bc9d5a6bfe7137daeb0c166b75b3
Janette Campbell <campbjp@hotmail.com>
2022-08-08 16:46:26 -0400

    feat: added scroll animation

--- js/script.js
+++ js/script.js
@@ -1,1 +1,21 @@
-const nav = document.getElementsByName("nav");
+const animate = document.querySelectorAll(".animate");
+
+const isVisible = (e) => {
+  const elementDiv = e.getBoundingClientRect();
+  console.log(window.innerHeight);
+  console.log(elementDiv.top);
+  let distanceFromTop = -300;
+  return elementDiv.top - window.innerHeight < distanceFromTop ? true : false;
+};
+
+const scanElements = (e) => {
+  animate.forEach((e) => {
+    if (isVisible(e)) {
+      e.classList.add("fade-in");
+    } else {
+      e.classList.remove("fade-in");
+    }
+  });
+};
+
+window.addEventListener("scroll", scanElements);
