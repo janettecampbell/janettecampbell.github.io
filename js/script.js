3d7a89380acc78cdc58a6ff12a53547915c5068e
Janette Campbell <campbjp@hotmail.com>
2022-09-02 15:04:04 -0400

    feat: added close menu on link click

--- js/script.js
+++ js/script.js
@@ -1,8 +1,13 @@
+"use strict";
+
 // ============= Declaration of elements =================
 const animate = document.querySelectorAll(".animate");
 const animateLeft = document.querySelectorAll(".animate-left");
 const animateRight = document.querySelectorAll(".animate-right");
 const navBtnImg = document.querySelector(".nav-btn-img");
+const menuBtn = document.getElementById("menu-btn");
+// const menuList = document.querySelector(".menu-list");
+const menuItem = document.querySelectorAll(".menu-item");
 const navBtnContent = document.querySelector(".nav-btn-content");
 const visible = document.querySelector(".visible");
 const hidden = document.querySelector(".hidden");
@@ -64,16 +69,29 @@ window.addEventListener("scroll", scanElementsFadeRight);
 // ============== Show menu on click ===================
 // =====================================================
 
-// ================ Switch from visible to not visible===================
-const handleClick = (e) => {
+// ================ Switch from visible to not visible menu click ===================
+const menuClick = (e) => {
   e.preventDefault();
   if (navBtnContent.classList.contains("hidden")) {
     navBtnContent.classList.remove("hidden");
     navBtnContent.classList.add("visible");
+    menuBtn.src = "./images/close-icon.png";
   } else {
     navBtnContent.classList.remove("visible");
     navBtnContent.classList.add("hidden");
+    menuBtn.src = "./images/menu-icon.png";
   }
 };
 
-navBtnImg.addEventListener("click", handleClick);
+navBtnImg.addEventListener("click", menuClick);
+
+//================ Switch from visible to not visible on link click ===============
+const linkClick = () => {
+  navBtnContent.classList.remove("visible");
+  navBtnContent.classList.add("hidden");
+  menuBtn.src = "./images/menu-icon.png";
+};
+
+menuItem.forEach((item) => {
+  item.addEventListener("click", linkClick);
+});
